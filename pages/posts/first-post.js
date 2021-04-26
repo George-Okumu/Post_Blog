e0b0cf924d66dc340a77eb6ee8061ed305173d02
George Okumu <gokumu12@gmail.com>
2021-04-26 20:38:30 +0300

    added metadata and image

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,13 +1,29 @@
 import Link from 'next/link'
+import Image from 'next/image'
+import Head from 'next/head'
 
 export default function FirstPost(){
     return (
         <>
+        <Head>
+            <title>Post Page</title>
+        </Head>
         <h1>This is my first Post</h1>
         <Link href="/">
             <a>Back Home</a>
         </Link>
+        
+        <Image
+          src="/public/images/geoface.jpg" // Route of the image file
+          height={144} // Desired size with correct aspect ratio
+          width={144} // Desired size with correct aspect ratio
+          alt="George"
+        />
         </>
+
     )
+   
+
+    
 
 }
