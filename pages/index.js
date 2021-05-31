0332f75ace7b3901c9dd8c29874248dc6fd27666
George Okumu <gokumu12@gmail.com>
2021-05-31 19:29:00 +0300

    fetched data from random MealDB api

--- pages/index.js
+++ pages/index.js
@@ -10,31 +10,44 @@ export default function Home() {
   return (
     <Layout>
       <Head>
+        {" "}
         <title>{siteTitle}</title>
       </Head>
       <section>
         <p>Hello, this is {name} </p>
         <p>
-          <b>{name}</b> is an innovative and proactive Front-End Developer, successful in developing
-          elegant, groundbreaking website and application designs for demanding
-          clients. Talented in project management, team leadership and
-          independent problem-solving. <br/>Highly organized multitasking with
-          expertise in scheduling projects, enhancing designs, learning and
-          verifying code. Prepared to offer my skills and abilities to the team.
+          <b>{name}</b> is an innovative and proactive Front-End Developer,
+          successful in developing elegant, groundbreaking website and
+          application designs for demanding clients. Talented in project
+          management, team leadership and independent problem-solving. <br />
+          Highly organized multitasking with expertise in scheduling projects,
+          enhancing designs, learning and verifying code. Prepared to offer my
+          skills and abilities to the team.
         </p>
-        <br/>
+        <br />
         <p>
-          For more information about my projects, here is my portfolio link: <br/> <a className="text-blue-900" href="https://george-okumu.github.io/myPortfolio/">{name} Portfolio</a>
+          For more information about my projects, here is my portfolio link:{" "}
+          <br />{" "}
+          <a
+            className="text-blue-900"
+            href="https://george-okumu.github.io/myPortfolio/"
+          >
+            {name} Portfolio
+          </a>
         </p>
       </section>
-      <br/>
+      <br />
 
       <footer>
-  
-        <a className="text-blue-900 pt-5" href="/posts/first-post">Check my Posts</a>
-
+        <a className="text-blue-900 pt-5" href="/posts/first-post">
+          Check my Posts
+        </a>
+        <li>
+        <a className="text-blue-900 pt-5" href="/mealdb">
+          Result of MealDB response
+        </a>
+        </li>
       </footer>
     </Layout>
-
   );
 }
